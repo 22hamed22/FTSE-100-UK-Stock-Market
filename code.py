a7ac51c80ab53d22b116e37ae54fa4798f504b48
hamed <100076148+22hamed22@users.noreply.github.com>
2025-01-29 23:52:31 +0000

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,14 @@ from itertools import cycle
 import streamlit as st
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
-import tensorflow 
+
+try:
+    import tensorflow as tf
+    st.write(f"TensorFlow version: {tf.__version__}")
+except ImportError as e:
+    st.write(f"Error importing TensorFlow: {e}")
+    
+#import tensorflow 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
 from sklearn.model_selection import train_test_split
