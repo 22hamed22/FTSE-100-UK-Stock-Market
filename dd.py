c74c5775bb48cf47597ab984c2de3f09ae3946ff
hamed <100076148+22hamed22@users.noreply.github.com>
2025-01-29 23:53:35 +0000

    Create dd.py

---
+++ dd.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import scikit_learn
+try:
+    import tensorflow as tf
+    st.write(f"TensorFlow version: {tf.__version__}")
+except ImportError as e:
+    st.write(f"Error importing TensorFlow: {e}")
